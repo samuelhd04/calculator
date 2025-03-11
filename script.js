2ce0ef1878446159e1a276b0a57eeacc46075a88
samuelhd04 <shernandezdu@unal.edu.co>
2025-03-10 22:17:57 -0500

    Create functions for each button

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, b, symbol) {
+  if (symbol === "+") {
+    return add(a, b);
+  } else if (symbol === "-") {
+    return subtract(a, b);
+  } else if (symbol === "*") {
+    return multiply(a, b);
+  } else if (symbol === "/") {
+    return divide(a, b);
+  }
+}
+
 const displayText = document.querySelector(".display p");
 const clearButton = document.querySelector(".clear");
 const deleteButton = document.querySelector(".delete");
+const buttons = document.querySelectorAll(".bottom-buttons button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    displayText.textContent = event.target.innerText;
+  });
+});
